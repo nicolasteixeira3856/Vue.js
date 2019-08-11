30a91bbc5a3850cd8c17a9d4147ae6a6dcaa74ae
nicolasteixeira3856 <nicolasteixeira3856@outlook.com>
2019-08-11 20:18:40 -0300

    Inicinado com Vue Components

---
+++ componentes/time.js
@@ -0,0 +1,53 @@
+class Time {
+    constructor(nome, escudo){
+        this.nome = nome;
+        this.escudo = escudo;
+        this.saldo = 0;
+        this.pontos = 0;
+        this.gm = 0;
+        this.gs = 0;
+    }
+    fimJogo(timeVisitante,golsMarcados,golsSofridos){
+        if (this.foiEmpate(golsMarcados,golsSofridos)){
+            this.empate(golsMarcados, golsSofridos);
+            timeVisitante.empate(golsMarcados, golsSofridos);
+            return;
+        }
+
+        if(this.foiVitoaria(golsMarcados, golsSofridos)){
+            this.vitoria(golsMarcados, golsSofridos);
+            timeVisitante.derrota(golsSofridos, golsMarcados);
+        }else{
+            this.derrota(golsMarcados, golsSofridos);
+            timeVisitante.vitoria(golsSofridos, golsMarcados);
+        }
+
+    }
+
+    foiEmpate(golsMarcados, golsSofridos){
+        return golsMarcados === golsSofridos
+    }
+
+    foiVitoaria(golsMarcados, golsSofridos){
+        return golsMarcados > golsSofridos;
+    }
+
+    empate(golsMarcados, golsSofridos){
+        this.atualizarInfo(1, golsMarcados, golsSofridos)
+    }
+
+    vitoria(golsMarcados, golsSofridos){
+        this.atualizarInfo(3, golsMarcados, golsSofridos)
+    }
+
+    derrota(golsMarcados, golsSofridos){
+        this.atualizarInfo(0, golsMarcados, golsSofridos)
+    }
+    
+    atualizarInfo(pontos, golsMarcados, golsSofridos){
+        this.pontos += pontos;
+        this.gm += golsMarcados;
+        this.gs += golsSofridos;
+        this.saldo += this.gm - this.gs;
+    }
+}
